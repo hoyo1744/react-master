98553fdabeb5b9841eb3d0a03f6351fc5b5d742c
hoyo1744 <hoyo1744@naver.com>
2025-01-11 23:35:17 +0900

    #2.2 Adapting and Extending

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,24 +1,30 @@
 
 import styled from "styled-components";
 
+// 2-1
 // `` 내부에 css 코드를 넣어준다.
 //  return <Father> 이렇게 적용한다. 더 간편하군?
 // styled.<html 태그>`css 코드`
+
+// 2-2
+// 중복된 코드를 제거하고 확장 가능한 styled-component 사용방법
 const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+// 컴포넌트도 props를 사용할 수 있다!
+// props는 데이터를 전송하는 방법!
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+// Box의 속성은 모두 들고와서 추가해준다. 마치 상속처럼!
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
+
 
 const Text = styled.span`
   color: white;
@@ -33,10 +39,8 @@ const Text = styled.span`
 
 function App() {
   return <Father>
-    <BoxOne>
-      <Text>Hello</Text>
-    </BoxOne>
-    <BoxTwo/>
+    <Box bgColor="teal"/>
+    <Circle bgColor="whitesmoke"/>
   </Father>
 }
 
