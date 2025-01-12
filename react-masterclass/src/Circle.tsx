beda4a1a2d6b65b98676769597cea2e1c6e05040
hoyo1744 <hoyo1744@naver.com>
2025-01-12 22:09:06 +0900

    #3.3 Optional Props interface CircleProps {     bgColor: string;     borderColor?: string;     text?: string; --> Optional props }

--- react-masterclass/src/Circle.tsx
+++ react-masterclass/src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
     bgColor: string;
+    borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,18 +10,25 @@ const Container = styled.div<ContainerProps>`
     height: 200px;
     background-color: ${ (props) => props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${ (props) => props.borderColor};
 `;
 
 // 이렇게 하면 bgColor의 타입이 뭔지 몰라서 빨간색 표시가난다.
 // 그래서 interface라는걸해줘야함.
 // interface는 object shape를 typeScripty에게 설명해주는 TypeScript의 개념 그전에는 a:number 이런식으로 타입을 지정함.
 
+// 3-3 Optional Props
+// ?를 붙여주면 필수가아님.
+// return <Container bgColor={bgColor} borderColor={borderColor ?? "white"}/>; ?? 이후는 디폴트값을 전달함. 삼항연산자와 같다.
+
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
 
-function Circle({bgColor}: CircleProps){
-    return <Container bgColor={bgColor}/>;
+function Circle({bgColor, borderColor, text = "default text"}: CircleProps){
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>{text}</Container>;
 }
 
 export default Circle;
