b04c4e3b7e40dc946975330e3b669ac515c07e23
hoyo1744 <hoyo1744@naver.com>
2025-01-12 16:06:22 +0900

    #2.7 Themes ThemeProvider 의 사용과 다크모드

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,64 +1,32 @@
 
 import styled, {keyframes} from "styled-components";
 
-// 2-5 style-component안에서 내부에서 특정 태그에 의존하고 싶지 않다면?
+// 2-7
+// theme 은 기본적으로 모든 색상을 가지고 있는 Object를 의미함.
+// color: ${props => props.theme.textColor};
+// index.js에서 ThemeProvider에서 지정한 Theme값을 하위 컴포넌트에서 가져와 사용할 수 있다.
+
+const Title = styled.h1`
+    color: ${props => props.theme.textColor};
+    
+`;
+
 const Wrapper = styled.div`
     display: flex;
     height: 100vh;
     width: 100vw;
     justify-content: center;
     align-items: center;
-`;
-
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-    
-  }
+    background-color: ${props => props.theme.backgroundColor};
 `;
 
 
 
-const Emoji = styled.span`
-    font-size: 36px;
-`;
 
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation:${animation} 1s linear infinite;
-  // Box안의 span까지 style을 Box에서 줄 수 있다. 다만 span이 아니라면? 지금 Box는 span에 의존하고 있다.
-  //  그럼 어떻게해야할까? html 태그가 아니라 style-component를 의존하면 되지 않을까?
-  //  아래처럼하면 Emoji에 의존하니까 html 태그가 변경되어도 상관없다.
-  ${Emoji} {
-    &:hover{
-        font-size: 98px;
-    }
-  }
-}
-  
-`;
 function App() {
   return (
       <Wrapper>
-        <Box>
-            <Emoji>😍</Emoji>
-        </Box>
-        <Emoji>💖</Emoji>
+        <Title>Hello</Title>
       </Wrapper>
   )
 
