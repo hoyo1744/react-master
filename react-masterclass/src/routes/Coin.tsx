f01aba13d40c5e69f580be7906a7a4e4870ba509
hoyo1744 <hoyo1744@naver.com>
2025-01-18 14:44:30 +0900

    #5.4 Route States
    -원한다면 link를 통해서 다른 화면에 정보를 보낼 수 있다.
    -코인페이지에서 새롭게 코인에 대한 정보를 요청하면 그 사이에 loading 화면이 나온다.
    -하지만 우린 적어도 코인의 이름을 알고 있다. 데이터를 이미 브라우저가 알고있다.
    -보이지 않게 데이터를 보낼 수 있다. 우리는 parameter를 통해 URL에게 코인 정보를 넘긴다. 이 방법으로 State를 사용할 수 있다.
    -Link 컴포넌트를 사용할 때 string 혹은 query arg를 보낼 수 있고, obj를 보낼 수 있다.
    -react router DOM이 보내주는 location에 접근하기 위해선 useLocation()을 사용한다.
    -누군가 id로 바로 접근하려하면 state를 찾을 수 없다고 나온다. 그 이유는 home에서 state를 받아오지 않았기 때문이다.

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -1,17 +1,66 @@
-import {useParams} from "react-router-dom";
+import {useState} from "react";
+import {useLocation, useParams} from "react-router-dom";
+import styled from "styled-components";
+
 
 // const {coinId} = useParams<{coinId:string}>(); 처럼 params으로 넘어온값이 어떤타입인지 타입스크립트에게 알려줘야함. 또는 인터페이스를 사용해도됨.
 // const {coinId} = useParams<Params>(); 같은 방법임.
 // 이게 무슨뜻이냐면. 타입스크립트에게 useParams가 coinId를 포함한 오브젝트를 반환할거라고 알려주는것.
 
+//https://v5.reactrouter.com/web/api/Link/to-object에서 보면 <Link>를 통해서 string뿐만 아니라 Object도 전달할 수 있음.
+
+const Title = styled.h1`
+    font-size: 48px;
+    text-align: center;
+    color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
 interface RouteParams {
     coinId: string;
 }
 
-function Coin() {
-    const {coinId} = useParams<RouteParams>();
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
 
-    return <h1>Coin : {coinId}</h1>
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+interface RouteState {
+    name: string;
+
+}
+
+function Coin() {
+    const [loading, setLoading] = useState(true);
+    const {coinId} = useParams<RouteParams>();
+    const {state} = useLocation<RouteState>();
+    console.log((state.name))
+
+
+
+    return (
+        <Container>
+            <Header>
+                {/*state값은 외부에서 전달한 값이기 때문에 바로 Coin 화면으로 접속하면 에러가 발생한다.
+                그래서 state? 라는 표현으로 삼항연산자를 표현한다.*/}
+                {/*<Title>{state?.name || "Loading"}</Title>*/}
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
