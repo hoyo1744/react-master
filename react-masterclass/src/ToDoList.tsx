a79c0cc59962e336f3656a31ef565e74c316d11d
hoyo1744 <hoyo1744@naver.com>
2025-01-27 15:10:06 +0900

    #6.8 Form Errors (
    정규표현식
    ^ 문장의 시작
    + 하나 또는 하나이상
    ```
    /^[A-Za-z0-9._%+-]+@naver.com$/
    /^[A-Za-z0-9._%+-]+@naver.com/g
    ```
    https://www.regexpal.com
    
    React Hook Form (TypeScript)
    React Hook Form은 TypeScript로 빌드되었으며, FormData 유형을 정의하여 form 값을 지원할 수 있습니다.
    ```
    type FormData = {
    firstName: string;
    lastName: string;
    };
    
    const { register, setValue, handleSubmit, formState: { errors } } = useForm< FormData >();
    ```
    https://react-hook-form.com/get-started#TypeScript
    
    defaultValues: Record< string, any > = {}
    input에 대한 defaultValues는 사용자가 component와 상호 작용하기 전에 component가 처음 렌더링될 때 초기 값으로 사용됩니다.
    모든 input에 대한 defaultValues를 빈 문자열이나 null과 같은 정의되지 않은 값으로 설정하는 것이 좋습니다.
    https://react-hook-form.com/api/useform#props

--- react-masterclass/src/ToDoList.tsx
+++ react-masterclass/src/ToDoList.tsx
@@ -5,47 +5,56 @@ import {useForm} from "react-hook-form";
 // function TodoList() {
 
 
+// react-hook-form을 사용하면 아래의 코드가 모두 1줄의 코드로 바뀐다.
+// const [toDo, setToDo] = useState("");
+//
+// const [toDoError, setTodoError] = useState("");
+//
+//
+//
+// // event.currentTarget.value 와 같다. 아래와 같이 사용하는 이유는 value 말고 다른 값도 추출할 경우에 유용하다.
+// const onChange=  (event:React.FormEvent<HTMLInputElement>) => {
+//     const {
+//         currentTarget: {value},
+//     } = event;
+//     setTodoError("")
+//     setToDo(value);
+// };
+//
+//
+// const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//
+//     if (toDo.length < 10) {
+//         return setTodoError("To do should be longer");
+//     }
+//
+//     console.log("submit");
+// };
+//
+//
+//
+// return <div>
+//     <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder="Write a todo "/>
+//         <button>Add</button>
+//         {toDoError !==  "" ? toDoError : null}
+//     </form>
+//
+// </div>
 
-    // react-hook-form을 사용하면 아래의 코드가 모두 1줄의 코드로 바뀐다.
-    // const [toDo, setToDo] = useState("");
-    //
-    // const [toDoError, setTodoError] = useState("");
-    //
-    //
-    //
-    // // event.currentTarget.value 와 같다. 아래와 같이 사용하는 이유는 value 말고 다른 값도 추출할 경우에 유용하다.
-    // const onChange=  (event:React.FormEvent<HTMLInputElement>) => {
-    //     const {
-    //         currentTarget: {value},
-    //     } = event;
-    //     setTodoError("")
-    //     setToDo(value);
-    // };
-    //
-    //
-    // const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault();
-    //     console.log(toDo);
-    //
-    //     if (toDo.length < 10) {
-    //         return setTodoError("To do should be longer");
-    //     }
-    //
-    //     console.log("submit");
-    // };
-    //
-    //
-    //
-    // return <div>
-    //     <form onSubmit={onSubmit}>
-    //         <input onChange={onChange} value={toDo} placeholder="Write a todo "/>
-    //         <button>Add</button>
-    //         {toDoError !==  "" ? toDoError : null}
-    //     </form>
-    //
-    // </div>
+interface IForm {
+    email: string,
+    firstName: string,
+    lastName : string,
+    userName : string,
+    password: string,
+    password1: string
+}
 
-function ToDoList(){
+
+function ToDoList() {
 
     // register가 onChange를 대신해준다.
     // watch는 form의 입력값들의 변환를 관찬할 수 있게 해준다.
@@ -55,9 +64,13 @@ function ToDoList(){
     // 즉, 첫번째 함수는 모든 validation을 지나고 나서야 호출됨.
     // formState는 에러 정보를 담고 있다.
 
-    const {register, watch, handleSubmit, formState} = useForm();
+    const {register, watch, handleSubmit, formState:{errors}} = useForm<IForm>({
+        defaultValues: {
+            email: "@naver.com",
+        }
+    });
 
-    const onValid = (data:any) => {
+    const onValid = (data: any) => {
         console.log(data);
     }
 
@@ -65,21 +78,47 @@ function ToDoList(){
     // console.log(register("toDo"))
     // console.log(watch());
 
-    console.log(formState.errors);
+    console.log(errors);
 
     return <div>
-        <form style={{display:"flex", flexDirection:"column"}}
+        <form style={{display: "flex", flexDirection: "column"}}
               onSubmit={handleSubmit(onValid)}>
             {/*아래처럼함으로써 register함수가 반환하는 객체 가져다가 input에 props로 전달해준다.*/}
-            <input {...register("email", {required: true})} placeholder="Email"/>
-            <input {...register("fistName", {required: true})} placeholder="First Name"/>
-            <input {...register("lastName", {required: true})} placeholder="Last Name"/>
-            <input {...register("username", {required: true,  minLength: 10})} placeholder="Username"/>
-            <input {...register("password", {required: true, minLength: 5})} placeholder="Password"/>
-            <input {...register("password1", {required: "Password is required", minLength: {
-                value:5,
-                    message:"Your password is too short"
-                }})} placeholder="Password1"/>
+            <input {...register("email", {
+                required: "Email is required.", pattern: {
+                    value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                    message: "Only naver.com emails allowed."
+                }
+            })}
+                   placeholder="Email"/>
+            <span>
+                {errors?.email?.message}
+            </span>
+            <input {...register("firstName", {required: "firstName  is required."})} placeholder="First Name"/>
+            <span>
+                {errors?.firstName?.message}
+            </span>
+            <input {...register("lastName", {required: "lastName  is required."})} placeholder="Last Name"/>
+            <span>
+                {errors?.lastName?.message}
+            </span>
+            <input {...register("userName", {required: "userName is required.", minLength: 10})} placeholder="Username"/>
+            <span>
+                {errors?.userName?.message}
+            </span>
+            <input {...register("password", {required: "password is required.", minLength: 5})} placeholder="Password"/>
+            <span>
+                {errors?.password?.message}
+            </span>
+            <input {...register("password1", {
+                required: "Password is required", minLength: {
+                    value: 5,
+                    message: "Your password is too short"
+                }
+            })} placeholder="Password1"/>
+            <span>
+                {errors?.password1?.message}
+            </span>
             <button>Add</button>
         </form>
 
