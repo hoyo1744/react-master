1a4fe6a499f2db39e62c60a441bb892d1270caf7
hoyo1744 <hoyo1744@naver.com>
2025-01-27 22:08:30 +0900

    #7.1 Set Selectors
    Selector
    
    set? - 이 속성이 설정되면 selector는 쓰기 가능한 상태를 반환한다. 첫번째 매개변수로 콜백 객체와 새로 입력 값이 전달된다. 사용자가 selector를 재설정할 경우 새로 입력 값은 T 타입의 값 또는 DefaultValue 타입의 객체일 수 있다. 콜백에는 다음이 포함된다.
    
    get 매개변수
    다른 atom이나 selector로부터 값을 찾는데 사용되는 함수. 이 함수는 selector를 주어진 atom이나 selector를 구독하지 않는다.
    
    set 매개변수
    업스트림 Recoil 상태의 값을 설정할 때 사용되는 함수. 첫 번째 매개변수는 Recoil state, 두 번째 매개변수는 새로운 값(newValue)이다. 새로운 값은 업데이트 함수나 재설정 액션을 전파하는 DefalutValue 객체일 수 있다.
    ```
    const proxySelector = selector({
    key: 'ProxySelector',
    get: ({get}) => ({...get(myAtom), extraField: 'hi'}),
    set: ({set}, newValue) => set(myAtom, newValue),
    });
    ```
    https://recoiljs.org/ko/docs/api-reference/core/selector/

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -6,18 +6,23 @@ import {useRecoilState, useRecoilValue} from "recoil";
 function App() {
 
     const [minutes, setMinutes] = useRecoilState(minuteState);
-    const hours = useRecoilValue(hourSelect);
+    // state가 아니더라도 selector도 똑같다. 첫번째  인자는 get의 결과, 두번째 함수는 set 함수
+    const [hours, setHours] = useRecoilState(hourSelect);
 
 
     const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
         // string to number
         setMinutes(+event.currentTarget.value);
-    }
+    };
+
+    const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    };
 
     return (
         <div>
             <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes"/>
-            <input value={hours} type="number" placeholder="Hours"/>
+            <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours"/>
         </div>
     );
 }
