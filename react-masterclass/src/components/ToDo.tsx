32213ea634b52f34df83a96a90e00af116461e36
hoyo1744 <hoyo1744@naver.com>
2025-01-27 18:31:26 +0900

    #6.13 Categories
    *단축평가*
    논리 연산의 결과를 결정하는 피 연산자를 타입 변환하지 않고 그대로 반환한다.
    Ex) 1
    'Cat' && 'Dog' // 'Dog' << 빈 문자열이 아닌 문자열은 Boolean 값 true로 변경될 수 있는 Truthy 값이다.
    논리곱 연산자는 두 개의 피 연산자가 모두 true로 평가될 때 true를 반환한다.
    논리곱 연산자는 좌항에서 우항으로 평가가 진행된다.
    첫 번째 피 연산자가 true로 평가된다면 두 번째 피 연산자까지 확인해야 표현식을 평가할 수 있다.
    이때 논리 연산의 결과를 결정하는 두 번째 피연산자 'Dog'를 반환한다.
    Ex2)
    false && ' 'Dog' // false
    첫 번째 피 연산자의 값이 Boolean 값인 false다.
    논리곱 연산에서 어느 한 쪽의 피 연산자 값이 false 일 경우 결과는 false다.
    따라서 논리 연산의 결과가 첫 번째 피 연산자에서 결정되었으므로,
    첫 번째 피 연산자인 false를 반환한다.

--- react-masterclass/src/components/ToDo.tsx
+++ react-masterclass/src/components/ToDo.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import {IToDo} from "../atoms";
+import {IToDo, toDosState} from "../atoms";
+import {useSetRecoilState} from "recoil";
 
-function ToDo({text}:IToDo) {
+function ToDo({text, category, id}:IToDo) {
+
+    const setToDos = useSetRecoilState((toDosState));
+
+    const onClick = (newCategory:IToDo["category"]) => {
+        console.log("i wanna to ", newCategory);
+    }
+
     return (
         <li>
             {text}
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {/*&& 이게 무슨 문법이지? -> 단축연산자*/}
+            {/*onClick에서 인자를 넘기기 위한 방법은 아래와 같음.*/}
+            {category !== "DOING" && <button onClick={() => onClick("DOING")}>DOING</button>}
+            {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
+            {category !== "DONE" && <button onClick={() => onClick("DONE")}>DONE</button>}
         </li>
     )
 }
