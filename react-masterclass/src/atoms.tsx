6bac583a3f754ee4908ce6b0b5001463a052cf26
hoyo1744 <hoyo1744@naver.com>
2025-01-27 19:42:09 +0900

    #6.16 Selectors part One
    Selectors
    
    Selector는 파생된 state(derived state)의 일부를 나타낸다.
    즉, 기존 state를 가져와서, 기존 state를 이용해 새로운 state를 만들어서 반환할 수 있다. 기존 state를 이용만할 뿐 변형시키지 않는다. derived state는 다른 데이터에 의존하는 동적인 데이터를 만들 수 있기 때문에 강력한 개념이다.
    ```
    const filteredTodoListState = selector({
    key: 'filteredTodoListState',
    get: ({get}) => {
    const filter = get(todoListFilterState);
    const list = get(todoListState);
    
    switch (filter) {
    case 'Show Completed':
    return list.filter((item) => item.isComplete);
    case 'Show Uncompleted':
    return list.filter((item) => !item.isComplete);
    default:
    return list;
    }
    },
    });
    ```
    filteredTodoListState는 내부적으로 2개의 의존성 todoListFilterState와 todoListState을 추적한다. 그래서 둘 중 하나라도 변하면 filteredTodoListState는 재 실행된다.
    
    https://recoiljs.org/ko/docs/basic-tutorial/selectors/
    https://recoiljs.org/ko/docs/api-reference/core/selector/

--- react-masterclass/src/atoms.tsx
+++ react-masterclass/src/atoms.tsx
@@ -1,4 +1,4 @@
-import {atom} from "recoil";
+import {atom, selector} from "recoil";
 
 export interface IToDo {
     text: string;
@@ -11,3 +11,17 @@ export const toDosState = atom<IToDo[]>({
     key: "toDo",
     default: [],
 });
+
+// selector의 요점은 atom 을 가져다가(atom은 단순한 값 또는 배열임) output을 변형해서 그 결과를 반환하는것.
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    // get 함수를 이용하면 selector내부로 atom을 가져올 수 있다. get(toDosState)처럼
+    get: ({get}) => {
+        const toDos = get(toDosState)
+        return [
+            toDos.filter(toDo => toDo.category === "TO_DO"),
+            toDos.filter(toDo => toDo.category === "DOING"),
+            toDos.filter(toDo => toDo.category === "DONE")
+        ];
+    }
+})
--- react-masterclass/src/components/ToDoList.tsx
+++ react-masterclass/src/components/ToDoList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {useRecoilValue} from "recoil";
-import {toDosState} from "../atoms";
+import {toDoSelector, toDosState} from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -9,21 +9,42 @@ import ToDo from "./ToDo";
 function ToDoList() {
 
     // useRecoilState의 첫번째 인자는 값, 두번째인자는 변경 함수를 제공함.
-    const toDos = useRecoilValue(toDosState);
-    console.log(toDos);
+    // const toDos = useRecoilValue(toDosState);
+    const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr/>
-            <CreateToDo />
+            <CreateToDo/>
+            <h2>To Do</h2>
             <ul>
-                {toDos.map(toDo =>
+                {toDo.map(toDo =>
                     // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
                     // 동일코드(Props가 동일해서 동작함)
                     <ToDo key={toDo.id} {...toDo} />
                 )}
             </ul>
+            <hr/>
+            <h2>Doing</h2>
+            <ul>
+                {doing.map(toDo =>
+                    // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+                    // 동일코드(Props가 동일해서 동작함)
+                    <ToDo key={toDo.id} {...toDo} />
+                )}
+            </ul>
+            <hr/>
+            <h2>Done</h2>
+            <ul>
+                {done.map(toDo =>
+                    // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+                    // 동일코드(Props가 동일해서 동작함)
+                    <ToDo key={toDo.id} {...toDo} />
+                )}
+            </ul>
+            <hr/>
+
         </div>
     )
 }
