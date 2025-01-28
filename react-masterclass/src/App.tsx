c85a671bbc5b8a8315a86cd12d9eb2bbd64a9dd5
hoyo1744 <hoyo1744@naver.com>
2025-01-28 12:05:05 +0900

    #7.4 Styles and Placeholders
    theme.ts
    bgColor: "#3F8CF2",
    boardColor: "#DADFE9",
    cardColor: "white",
    
    provided.placeholder (?ReactElement)
    Draggable 엘리먼트를 드래그하는 동안 position: fixed(영역을 고정시킴)를 적용합니다.
    Draggable을 드래그할 때 Droppable 리스트가 작아지는 것을 방지하기 위해 필요합니다.
    Draggable 노드의 형제로 렌더링하는 것이 좋습니다.
    
    provided
    https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md#1-provided-droppableprovided

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,5 +1,43 @@
 import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd"
 import React from "react";
+import styled from "styled-components";
+
+
+const Board = styled.div`
+    background-color: ${(props) => props.theme.boardColor};
+    padding: 20px 10px;
+    padding-top: 30px;
+    border-radius: 5px;
+    min-height: 200px;
+`;
+
+const Boards = styled.div`
+    display: grid;
+    width: 100%;
+    grid-template-columns: repeat(1, 1fr);
+    
+`;
+
+const Card = styled.div`
+    background-color: ${(props) => props.theme.cardColor};
+    padding: 10px 10px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+`;
+
+
+const Wrapper = styled.div`
+    display: flex;
+    max-width: 480px;
+    width: 100%;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
+
 
 function App() {
     const onDragEnd = () => {
@@ -8,33 +46,28 @@ function App() {
     return (
         // DragDropContext는 드래그앤드롭을 수행할 곳에 적어줘야함. onDragEnd는 드래그앤듣롭이 끝나고 호출될 함수를 명시하는것,
         <DragDropContext onDragEnd={onDragEnd}>
-            <div>
-                <Droppable droppableId="one">
-                    {
-                        (magic) =>
-                            <ul ref={magic.innerRef} {...magic.droppableProps}>
-                                <Draggable draggableId="first" index={0}>
-                                    {/*기본적으로 요소가 드래그 되기를 원한다면 draggableProps를 넣어주면 된다.*/}
-                                    {/*dragHandle은 드래그의 트리거를 말한다. 즉, 어느위치에서든 드래그가 되기를 원한다면 이걸 써야함.*/}
+            <Wrapper>
+                <Boards>
+                    <Droppable droppableId="one">
+                        {
+                            (magic) =>
+                                <Board ref={magic.innerRef} {...magic.droppableProps}>
+                                    {toDos.map((toDo, index) => <Draggable draggableId={toDo} index={index}>
+                                        {/*기본적으로 요소가 드래그 되기를 원한다면 draggableProps를 넣어주면 된다.*/}
+                                        {/*dragHandle은 드래그의 트리거를 말한다. 즉, 어느위치에서든 드래그가 되기를 원한다면 이걸 써야함.*/}
 
-                                    {(magic) => <li
-                                        ref={magic.innerRef} {...magic.draggableProps} >
-                                        {/*dragHandleProps 있는 부분만 드래그가 가능함.*/}
-                                        <span {...magic.dragHandleProps}>😅</span>
-                                        One
-                                    </li>}
-                                </Draggable>
-                                <Draggable draggableId="second" index={1}>
-                                    {(magic) => <li
-                                        ref={magic.innerRef} {...magic.draggableProps} >
-                                        <span {...magic.dragHandleProps}>😅</span>
-                                        Two
-                                    </li>}
-                                </Draggable>
-                            </ul>
-                    }
-                </Droppable>
-            </div>
+                                        {(magic) => <Card
+                                            ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps} >
+                                            {/*dragHandleProps 있는 부분만 드래그가 가능함.*/}
+                                            {toDo}
+                                        </Card>}
+                                    </Draggable>)}
+                                    {magic.placeholder}
+                                </Board>
+                        }
+                    </Droppable>
+                </Boards>
+            </Wrapper>
         </DragDropContext>
     )
 }
