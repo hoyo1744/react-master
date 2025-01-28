459e2c1d9daff9b7ba6406ec959f11d62bf4e195
hoyo1744 <hoyo1744@naver.com>
2025-01-28 11:27:49 +0900

    #7.3 Drag and Drop part Two
    Using innerRef
    (Draggable과 Droppable컴포넌트의 내부 props정의)
    < Draggable /> 및 < Droppable /> 컴포넌트 모두 HTMLElement를 제공해야 합니다. 이것은 DraggableProvided 및 DroppableProvided 객체의 innerRef 속성을 사용하여 수행됩니다.
    https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/guides/using-inner-ref.md#using-innerref
    
    dragHandleProps
    특정 영역을 통해서만 드래그를 가능하도록 하고 싶을 때 사용한다.
    ex) {...provided.dragHandleProps}
    
    DragDropContext
    https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/drag-drop-context.md
    
    Droppable
    https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md
    
    Draggable
    https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/draggable.md

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -2,7 +2,8 @@ import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd"
 import React from "react";
 
 function App() {
-    const onDragEnd = () => {};
+    const onDragEnd = () => {
+    };
 
     return (
         // DragDropContext는 드래그앤드롭을 수행할 곳에 적어줘야함. onDragEnd는 드래그앤듣롭이 끝나고 호출될 함수를 명시하는것,
@@ -10,14 +11,27 @@ function App() {
             <div>
                 <Droppable droppableId="one">
                     {
-                        () => <ul>
-                            <Draggable draggableId="first" index={0}>
-                                {() => <li>One</li>}
-                            </Draggable>
-                            <Draggable draggableId="second" index={1}>
-                                {() => <li>Two</li>}
-                            </Draggable>
-                        </ul>
+                        (magic) =>
+                            <ul ref={magic.innerRef} {...magic.droppableProps}>
+                                <Draggable draggableId="first" index={0}>
+                                    {/*기본적으로 요소가 드래그 되기를 원한다면 draggableProps를 넣어주면 된다.*/}
+                                    {/*dragHandle은 드래그의 트리거를 말한다. 즉, 어느위치에서든 드래그가 되기를 원한다면 이걸 써야함.*/}
+
+                                    {(magic) => <li
+                                        ref={magic.innerRef} {...magic.draggableProps} >
+                                        {/*dragHandleProps 있는 부분만 드래그가 가능함.*/}
+                                        <span {...magic.dragHandleProps}>😅</span>
+                                        One
+                                    </li>}
+                                </Draggable>
+                                <Draggable draggableId="second" index={1}>
+                                    {(magic) => <li
+                                        ref={magic.innerRef} {...magic.draggableProps} >
+                                        <span {...magic.dragHandleProps}>😅</span>
+                                        Two
+                                    </li>}
+                                </Draggable>
+                            </ul>
                     }
                 </Droppable>
             </div>
