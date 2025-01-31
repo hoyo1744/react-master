0efc642b4d645d38b1855fc7cc15198faffe1267
hoyo1744 <hoyo1744@naver.com>
2025-01-31 16:59:16 +0900

    #8.5 Gestures part One
    Hover
    hover 제스처는 포인터가 컴포넌트 위로 이동하거나 컴포넌트를 떠날 때를 감지합니다. onMouseEnter 및 onMouseLeave와는 달리 실제 마우스 이벤트의 결과로만 호버가 실행되도록 보장됩니다.
    
    whileHover: VariantLabels | TargetAndTransition
    호버 제스처가 인식되는 동안 애니메이션할 속성 또는 변형 레이블입니다.
    < motion.div whileHover={{ scale: 0.8 }} / >
    https://www.framer.com/docs/gestures/#hover
    
    Tap
    whileTap: VariantLabels | TargetAndTransition
    컴포넌트를 누르고 있는 동안 애니메이션할 속성 또는 변형 레이블입니다.
    < motion.div whileTap={{ scale: 0.8 }} / >
    https://www.framer.com/docs/gestures/#tap
    
    Drag
    drag: boolean | "x" | "y"
    이 요소에 대해 끌기를 활성화합니다. 기본적으로 false로 설정됩니다. 양방향으로 드래그하려면 true로 설정하십시오. 특정 방향으로만 드래그하려면 "x" 또는 "y"를 설정합니다.
    < motion.div drag="x" / >
    
    whileDrag: VariantLabels | TargetAndTransition
    드래그 제스처가 인식되는 동안 애니메이션할 속성 또는 변형 레이블입니다.
    < motion.div whileDrag={{ scale: 1.2 }} / >
    https://www.framer.com/docs/gestures/#drag
    
    rgb(46, 204, 113)

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -13,52 +13,23 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    background-color: rgba(255, 255, 255, 0.2);
+    //display: grid;
+    //grid-template-columns: repeat(2, 1fr);
+    background-color: rgba(255, 255, 255, 1);
     border-radius: 15px;
     text: "tt";
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-    background-color: white;
-    height: 70px;
-    width: 70px;
-    place-self: center;
-    border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-    start: {
-        opacity: 0,
-        scale: 0.5,
-
-
-    },
-    end: {
-        scale: 1,
-        opacity: 1,
+    hover: {scale: 1.5, rotateZ: 90},
+    click: {scale: 1, borderRadius: "100px"},
+    drag:{
+        backgroundColor: "rgb(46, 204, 113)",
         transition: {
-            type: "spring",
-            duration: 0.5,
-            bounce: 0.5,
-            delayChildren: 0.5,
-            staggerChildren: 0.2,
-        },
+            duration: 10,
+        }
 
-    }
-}
-
-const circleVariants = {
-    start: {
-        opacity: 0,
-        y:10
-    },
-    end: {
-        opacity: 1,
-        y:0
     }
 }
 
@@ -68,12 +39,15 @@ function App() {
             {/*varients는 기본적으로 자식들에게 기본적으로 제공됨. 마치 상속처럼*/}
             {/*그래서 자식컴포넌트들은 initial과 animate에 기본값으로 부모값이 드렁감.*/}
             {/*즉, Box의 애니메이션이 기본적으로 CIrcle에도 적용될것임. 기본값으로 !*/}
-            <Box variants={boxVariants} initial="start" animate="end">
-                {/*자식은 variants만 다르고 이니셜과 애니메이트는 동일하게 부모와동일한 이름을 사용하도록 되어있음.*/}
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
+            <Box
+                drag
+                variants={boxVariants}
+                whileHover="hover"
+                whileDrag="drag"
+                whileTap="click"
+
+            >
+
             </Box>
         </Wrapper>
     );
