26e152becbd5f2d277f8e5c2cc50199723c8237b
hoyo1744 <hoyo1744@naver.com>
2025-01-31 16:44:04 +0900

    #8.4 Variants part Two
    Orchestration
    delayChildren: 딜레이 시간(초) 후에 하위 애니메이션이 시작됩니다.
    staggerChildren: 하위 컴포넌트의 애니메이션에 지속 시간(초)만큼 시차를 둘 수 있습니다. 예를 들어, staggerChildren이 0.01이면 첫 번째 자식은 0초, 두 번째 자식은 0.01초, 세 번째 자식은 0.02초 지연되는 식입니다. 계산된 stagger 딜레이가 delayChildren에 추가됩니다.
    https://www.framer.com/docs/transition/#orchestration
    
    inherit: boolean
    부모로부터 variant 변경 사항을 상속하지 않도록 하려면 false로 설정합니다.
    
    custom: any
    각 애니메이션 컴포넌트에 대해 dynamic variants을 다르게 사용할 사용자 지정 데이터입니다.
    ```
    const variants = {
    visible: (custom) => ({
    opacity: 1,
    transition: { delay: custom * 0.2 }
    })
    }
    
    < motion.div inherit={false} custom={0} animate="visible" variants={variants} / >
    < motion.div custom={1} animate="visible" variants={variants} / >
    < motion.div custom={2} animate="visible" variants={variants} / >
    ```
    https://www.framer.com/docs/component/###inherit
    
    place-items (Container Properties)
    justify-items과 align-items를 합친 축약형
    
    place-self (Item Properties)
    justify-self와 align-self를 합친 축약형

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,41 +1,80 @@
 import styled from "styled-components";
-import { motion } from "framer-motion"
-import {correctBorderRadius} from "framer-motion/types/projection";
+import {motion} from "framer-motion"
 
 const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 // 스타일컴포넌트에 애니메이션을 적용하기 위한 방법
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 15px;
-  text: "tt";
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+    width: 200px;
+    height: 200px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 15px;
+    text: "tt";
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-    start : {scale:0},
-    end : {scale: 1, rotateZ:360, transition: {type:"spring", delay:0.5}}
+const Circle = styled(motion.div)`
+    background-color: white;
+    height: 70px;
+    width: 70px;
+    place-self: center;
+    border-radius: 40px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+    start: {
+        opacity: 0,
+        scale: 0.5,
+
+
+    },
+    end: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            duration: 0.5,
+            bounce: 0.5,
+            delayChildren: 0.5,
+            staggerChildren: 0.2,
+        },
+
+    }
+}
+
+const circleVariants = {
+    start: {
+        opacity: 0,
+        y:10
+    },
+    end: {
+        opacity: 1,
+        y:0
+    }
 }
 
 function App() {
     return (
         <Wrapper>
-            {/*variants를 적어주면 intiail에서 animate에 적어준걸 찾아서 수행한다.*/}
-            <Box variants={myVars} initial="start"
-                animate="end"
-                // 위 코드와 같은 동작이다.
-                // initial={{scale:0}}
-                //  animate={{scale: 1, rotateZ: 360}}
-                //  transition={{type:"spring", delay: 0.5}}
-            />
+            {/*varients는 기본적으로 자식들에게 기본적으로 제공됨. 마치 상속처럼*/}
+            {/*그래서 자식컴포넌트들은 initial과 animate에 기본값으로 부모값이 드렁감.*/}
+            {/*즉, Box의 애니메이션이 기본적으로 CIrcle에도 적용될것임. 기본값으로 !*/}
+            <Box variants={boxVariants} initial="start" animate="end">
+                {/*자식은 variants만 다르고 이니셜과 애니메이트는 동일하게 부모와동일한 이름을 사용하도록 되어있음.*/}
+                <Circle variants={circleVariants}/>
+                <Circle variants={circleVariants}/>
+                <Circle variants={circleVariants}/>
+                <Circle variants={circleVariants}/>
+            </Box>
         </Wrapper>
     );
 }
