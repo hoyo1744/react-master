05079620884e329a122f1fc1f03d444a60649062
hoyo1744 <hoyo1744@naver.com>
2025-02-01 14:33:14 +0900

    #8.8 MotionValues part Two
    useTransform
    
    useTransform 훅을 통해 MotionValues를 연결합니다.
    useTransform()는 한 값 범위에서 다른 값 범위로 매핑하여 다른 MotionValue의 output을 변환하는 MotionValue를 만듭니다.
    x(Motion Value)값을 다른 output값으로 변환해준다.
    ex) x: -400 => 1
    ```
    const x = useMotionValue(0)
    const input = [-200, 0, 200]
    const output = [0, 1, 0]
    const opacity = useTransform(x, input, output)
    
    return < motion.div drag="x" style={{ x, opacity }} />
    ```
    https://www.framer.com/docs/motionvalue/##usetransform

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {motion, useMotionValue} from "framer-motion"
+import {motion, useMotionValue, useTransform} from "framer-motion"
 import {useEffect, useRef} from "react";
 
 const Wrapper = styled.div`
@@ -38,7 +38,7 @@ const BiggerBox = styled.div`
     height: 600px;
     background-color: rgba(255, 255, 255, 0.4);
     border-radius: 40px;
-    display: flex;
+    display: flex; 
     justify-content: center;
     align-items: center;
     overflow: hidden;
@@ -49,8 +49,9 @@ function App() {
     const biggerBoxRef = useRef<HTMLDivElement>(null)
 
     const x = useMotionValue(0)
+    const potato = useTransform(x, [-800, 0, 800], [2,1 ,0.1])
     useEffect(() => {
-        x.onChange( () => console.log(x.get()))
+        potato.onChange( () => console.log(potato.get()))
     }, [x])
 
     return (
@@ -58,10 +59,10 @@ function App() {
             {/*<BiggerBox ref={biggerBoxRef}>*/}
             {/*varients는 기본적으로 자식들에게 기본적으로 제공됨. 마치 상속처럼*/}
             {/*그래서 자식컴포넌트들은 initial과 animate에 기본값으로 부모값이 드렁감.*/}
-            <button onClick={() => x.set(200)}>click me</button>
+            {/*<button onClick={() => x.set(200)}>click me</button>*/}
             {/*즉, Box의 애니메이션이 기본적으로 CIrcle에도 적용될것임. 기본값으로 !*/}
             <Box
-                style={{x}}
+                style={{x, scale: potato}}
                 drag="x"
                 dragSnapToOrigin
                 // drag
