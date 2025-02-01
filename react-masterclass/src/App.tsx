a9b0acba83965f05704e4a6c893f5d514beafbb5
hoyo1744 <hoyo1744@naver.com>
2025-02-01 15:15:26 +0900

    #8.10 SVG Animation
    Line drawing
    svg 엘리먼트에 'pathLength', 'pathSpacing', 'pathOffset' 속성을 사용하여 Line drawing 애니메이션을 만들 수 있습니다.
    https://www.framer.com/docs/examples/#line-drawing
    
    path (SVG)
    path SVG 엘리먼트는 모양을 정의하는 일반 엘리먼트입니다.모든 기본 모양은 path 엘리먼트로 만들 수 있습니다.
    path의 속성 d는 경로의 모양을 정의합니다.
    https://developer.mozilla.org/en-US/docs/Web/SVG/Element/path
    
    Path
    motion.path 컴포넌트는 세 가지 강력한 SVG path 속성인 pathLength, pathSpacing 및 pathOffset을 가지고 있습니다. 수동 경로 측정이 필요 없이 모두 0과 1 사이의 값으로 설정됩니다.
    
    Line drawing
    선 그리기 애니메이션은 pathLength, pathSpacing 및 pathOffset의 세 가지 특수 속성을 사용하여 많은 SVG 요소로 만들 수 있습니다.
    ex) motion.circle initial={{ pathLength: 0 }} animate={{ pathLength: 1 }}
    https://www.framer.com/docs/examples/#line-drawing

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -3,12 +3,12 @@ import {motion, useMotionValue, useTransform, useViewportScroll} from "framer-mo
 import {useEffect, useRef} from "react";
 
 const Wrapper = styled(motion.div)`
-    height: 200vh;
+    height: 100vh;
     width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: linear-gradient(135deg, rgb(238,0,153), rgb(238, 0, 153));
+    background: linear-gradient(135deg, rgb(238, 0, 153), rgb(238, 0, 153));
 `;
 
 // 스타일컴포넌트에 애니메이션을 적용하기 위한 방법
@@ -25,7 +25,7 @@ const Box = styled(motion.div)`
 const boxVariants = {
     hover: {scale: 1.5, rotateZ: 90},
     click: {scale: 1, borderRadius: "100px"},
-    drag:{
+    drag: {
         backgroundColor: "rgb(46, 204, 113)",
         transition: {
             duration: 10,
@@ -39,55 +39,48 @@ const BiggerBox = styled.div`
     height: 600px;
     background-color: rgba(255, 255, 255, 0.4);
     border-radius: 40px;
-    display: flex; 
+    display: flex;
     justify-content: center;
     align-items: center;
     overflow: hidden;
 `;
 
+const Svg = styled.svg`
+    width: 300px;
+    color: white;
+    height: 300px;
+`;
+
+const svg = {
+    start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+    end: {
+        fill: "rgba(255, 255, 255, 1)",
+        pathLength: 1,
+    },
+};
+
 function App() {
-
-    const biggerBoxRef = useRef<HTMLDivElement>(null)
-
-    const x = useMotionValue(0)
-    const rotate = useTransform(x, [-800, 800], [-360, 360])
-
-    const gradient = useTransform(x, [-800, 800], [
-        "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-        "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ])
-
-    const {scrollY, scrollYProgress} = useViewportScroll();
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
-    useEffect(() => {
-        scrollY.onChange(() => {console.log(scrollY.get(), scrollYProgress.get())})
-
-    }, [scrollY, scrollYProgress])
-
     return (
-        <Wrapper style={{background: gradient}}>
-            {/*<BiggerBox ref={biggerBoxRef}>*/}
-            {/*varients는 기본적으로 자식들에게 기본적으로 제공됨. 마치 상속처럼*/}
-            {/*그래서 자식컴포넌트들은 initial과 animate에 기본값으로 부모값이 드렁감.*/}
-            {/*<button onClick={() => x.set(200)}>click me</button>*/}
-            {/*즉, Box의 애니메이션이 기본적으로 CIrcle에도 적용될것임. 기본값으로 !*/}
-            <Box
-                style={{x, rotate, scale}}
-                drag="x"
-                dragSnapToOrigin
-                // drag
-                // dragConstraints={biggerBoxRef}
-                // dragSnapToOrigin
-                // dragElastic={0}
-                // variants={boxVariants}
-                // whileHover="hover"
-                // whileDrag="drag"
-                // whileTap="click"
-            >
+        <Wrapper>
+            <Svg
+                xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 448 512">
+                <motion.path
+                    variants={svg}
+                    initial="start"
+                    animate="end"
+                    transition={{
+                        // default는 모든 속성에 부여함.
+                        default: {duration: 5},
+                        fill: {duration: 1, delay: 3}
+                    }}
+                    stroke="white"
+                    strokeWidth="5"
+                    fill="transparent"
+                    d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"/>
+            </Svg>
 
-            </Box>
-            {/*</BiggerBox>*/}
+
         </Wrapper>
     );
 }
